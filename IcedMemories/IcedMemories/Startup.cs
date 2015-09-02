6bdea193f62e1dc50a6403644dcea891820cb26f
Martin Keeton <martin.keeton79@gmail.com>
2015-09-02 14:57:45 +0100

    Initial Commit

---
+++ IcedMemories/IcedMemories/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IcedMemories.Startup))]
+namespace IcedMemories
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
