dd4b2849f05d3b8252d9ef656549676c2299b691
Martin Keeton <mkeeton@Highfieldabc.com>
2015-12-10 07:55:00 +0000

    Reworking to include SSO in main site.

---
+++ ISSolutions/ISSolutions.Home/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ISSolutions.Home.MvcApplication" Language="C#" %>
---
+++ ISSolutions/ISSolutions.Home/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ISSolutions.Home.Startup))]
+namespace ISSolutions.Home
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
